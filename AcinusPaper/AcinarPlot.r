6bef9e850f0c424a68110f03c1ef302d66743b29
(no author) <(no author)@d6103094-951f-4e2d-9b0d-013b0bede56b>
2011-03-10 16:51:19 +0000

    - improved a lot of plotting commands, R now gives out some details of the Data, like # of Acini and the increase - Datafiles updated with new Data from AcinusExtraction-XLS-Files
    git-svn-id: http://code.ana.unibe.ch/svn/R@3 d6103094-951f-4e2d-9b0d-013b0bede56b

--- AcinusPaper/AcinarPlot.r
+++ AcinusPaper/AcinarPlot.r
@@ -12,33 +12,64 @@ library(beanplot) # load Beanplot library (http://www.jstatsoft.org/v28/c01/)
 Data.original <-read.csv("AcinusExtractionDataFile.csv",head=TRUE,sep=";")
 # Draw original Data
 boxplot(Data.original,
-  main="Boxplot of Acinar Volumes, including outliers, but not drawing them",
+  main="Boxplot of Acinar Volumes, original data",
+  xlab="Days after birth",
+  notch=TRUE, # If the notches of two plots do not overlap this is "strong evidence" that the two medians differ
+  varwidth=TRUE, # scale width of boxes to amount of samples
+  col="lightgray"
+  )
+boxplot(Data.original,
+  main="Boxplot of Acinar Volumes, original data, without drawing the outliers",
   xlab="Days after birth",
   notch=TRUE, # If the notches of two plots do not overlap this is "strong evidence" that the two medians differ
   varwidth=TRUE, # scale width of boxes to amount of samples
   col="lightgray",
-  outline=FALSE)
-beanplot(Data.original,
-  main="Beanplot of Acinar Volumes, original data",
-  col="lightgray")
+  outline=FALSE)  
+boxplot(Data.original[,1:2],
+  main="Only Day 4 and 10, to see the difference",
+  notch=TRUE,
+  outline=FALSE) # Plot only Day 4 and 10 to see the difference  
+# beanplot(Data.original,
+#  main="Beanplot of Acinar Volumes, original data",
+#  col="lightgray")
 summary(Data.original)
 
-# Identified outliers, modified data (removed largest 25 acini of each day
-# and saved as new .csv
+# Sorted AcinusExtractionDataFile in descending size order.
+# Saved as AcinusExtractionDataFileWithoutOutliers.csv
+# Iteratively used boxplo(Data[,column] to identified outliers, delete them, save file and plot again until no outliers are shown anymore.
+# Repteat for each day, then you've got Data-File without outliers
 Data<-read.csv("AcinusExtractionDataFileWithoutOutliers.csv",head=TRUE,sep=";")
+# boxplot(Data[,5])
 # Give out Details of Data
 summary(Data)
+# Give out Details
+Data.mean <- mean(Data,na.rm=TRUE)
+Data.sd <- sd(Data,na.rm=TRUE)
+# Increase of Volume compared to Day 4
+Data.mean/Data.mean[1]
+# How many Volumes?
+length(na.exclude(Data[,1]))
+length(na.exclude(Data[,2]))
+length(na.exclude(Data[,3]))
+length(na.exclude(Data[,4]))
+length(na.exclude(Data[,5]))
+
 # Plot the Data
 boxplot(Data,
   varwidth=TRUE,
   notch=TRUE,
   col="lightgray",
-  main="Boxplot of Acinar Volumes with removed outliers",
+  main="Boxplot of Acinar Volumes without outliers",
   xlab="Days after birth"
-  ) 
-beanplot(Data,
-  main="Beanplot of acinar volumes with removed outliers",
-  col="lightgray")
+  )
+boxplot(Data[,1:2],
+  main="Only Day 4 and 10, without outliers, to see the difference",
+  notch=TRUE) # Plot only Day 4 and 10 to see the difference
+boxplot(Data[,1:3],notch=TRUE)
+# beanplot(Data,
+#  main="Beanplot of acinar volumes with removed outliers",
+#  col="lightgray")
+
 
 # Plot single days
 par(mfrow=c(5,1))
@@ -54,6 +85,14 @@ par(mar=c(5,4,4,2)+0.1) # set margins back to default
 # Calculate Mean and Standard Deviation
 Data.mean <- mean(Data,na.rm=TRUE)
 Data.sd <- sd(Data,na.rm=TRUE)
+days<-c(4,10,21,36,60)
+plot(days,Data.mean,
+  type="b", # both line and dots
+  main="Mean of the Data over the postnatal lung development. Arrows show SD",
+  xlab="Days after birth",
+  ylab="Mean acinar volume [microliter]")
+points(days,Data.mean,col="red")
+# arrows(days, Data.mean - Data.sd, days, Data.mean + Data.sd,code = 3, col = "red") # Draw Arrows with +- SD
 
 # Shift data to the right, if desired
 boxplot(Data,
@@ -64,20 +103,16 @@ boxplot(Data,
   xlab="Days after birth"
   )
 Data.shifted <- seq(Data) + 0.25
-par(mfrow=c(2,1))
-par(mar=c(10, 5, 4, 2))
-x <- 1:10
-plot(x)
-box("figure", lty="dashed")
-par(mar=c(5, 5, 10, 2))
-plot(x)
-box("figure", lty="dashed")points(Data.shifted, Data.mean, col = "red") # Draw Means into Boxplot
+points(Data.shifted, Data.mean, col = "red") # Draw Means into Boxplot
 arrows(Data.shifted, Data.mean - Data.sd, Data.shifted, Data.mean + Data.sd,code = 3, col = "red") # Draw Arrows with +- SD
 
 ## save to TikZ-Graphics, ready for LaTeX
 # require(tikzDevice)
 # tikz('BoxPlotAcini.tex', standAlone = TRUE, width=5, height=5)
-# boxplot(Data)
+#   boxplot(Data,varwidth=TRUE,notch=TRUE,col="lightgray",main="Boxplot of Acinar Volumes",xlab="Days after birth")
+#   Data.shifted <- seq(Data) + 0.25
+#   points(Data.shifted, Data.mean, col = "red") # Draw Means into Boxplot
+#   arrows(Data.shifted, Data.mean - Data.sd, Data.shifted, Data.mean + Data.sd,code = 3, col = "red") # Draw Arrows with +- SD
 # dev.off()
 
 ## ANOVA (without knowing what I do...)
@@ -85,16 +120,16 @@ arrows(Data.shifted, Data.mean - Data.sd, Data.shifted, Data.mean + Data.sd,code
 # plot(fit)
 
 ## T-Test (the same results as =ttest(d4,d10,2,3) in Excel)
-t.test(Data$D4,Data$D10)
-t.test(Data$D4,Data$D21)
-t.test(Data$D4,Data$D36)
-t.test(Data$D4,Data$D60)
-
-t.test(Data$D10,Data$D21)
-t.test(Data$D10,Data$D36)
-t.test(Data$D10,Data$D60)
-
-t.test(Data$D21,Data$D36)
-t.test(Data$D21,Data$D60)
-
-t.test(Data$D36,Data$D60)
+# t.test(Data$D4,Data$D10)
+# t.test(Data$D4,Data$D21)
+# t.test(Data$D4,Data$D36)
+# t.test(Data$D4,Data$D60)
+# 
+# t.test(Data$D10,Data$D21)
+# t.test(Data$D10,Data$D36)
+# t.test(Data$D10,Data$D60)
+# 
+# t.test(Data$D21,Data$D36)
+# t.test(Data$D21,Data$D60)
+# 
+# t.test(Data$D36,Data$D60)
