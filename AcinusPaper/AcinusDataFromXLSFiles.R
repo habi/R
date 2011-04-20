e1d5ac86c5acab11d824461b1786325c24d90324
(no author) <(no author)@d6103094-951f-4e2d-9b0d-013b0bede56b>
2011-04-20 08:25:56 +0000

    - Added a for-loop to AcinusDataFromXLSFiles.R, so all the Data is extracted and plotted in one go. Additionally, now the graphs are titled nicely.
    git-svn-id: http://code.ana.unibe.ch/svn/R@8 d6103094-951f-4e2d-9b0d-013b0bede56b

--- AcinusPaper/AcinusDataFromXLSFiles.R
+++ AcinusPaper/AcinusDataFromXLSFiles.R
@@ -7,25 +7,30 @@
 setwd("p:/doc/#R/AcinusPaper")
 library(gdata)
 
-Day <- 10
-Day <- sprintf("%02.0f", Day)
-FileName <- paste("R108C",Day,".xls",sep="")
-FileLocation <- paste("p:/doc/#Tables/AcinarTreeExtraction/",FileName,sep="")
-Names <- sheetNames(FileLocation)
+Days <- c(04,10,21,36,60)
+for (currentDay in 1:length(Days)) {
+  Day <- Days[currentDay]
+  Day
+  Day <- sprintf("%02.0f", Day)
+  FileName <- paste("R108C",Day,".xls",sep="")
+  FileLocation <- paste("p:/doc/#Tables/AcinarTreeExtraction/",FileName,sep="")
+  Names <- sheetNames(FileLocation)
 
-cat(FileName, "contains", sheetCount(FileLocation), "Sheets with the following names:\n")
-for (i in 1:length(Names)) print(Names[i])
+  cat(FileName, "contains", sheetCount(FileLocation), "Sheets with the following names:\n")
+  for (i in 1:length(Names)) print(Names[i])
 
-for (i in 2:length(Names)) {
-  cat("reading Sheet #",i,"\n")
-  Data <- read.xls(FileLocation,sheet=i) #read Sheet Nr. i
-  Data <- Data[,2] # only keep Volumes (in the second column)
-  Mean <- mean(Data,na.rm=TRUE) # Mean
-  if (!is.nan(Mean)) {
-    plot(Data,main=Names[i])
-    lines(Data)
-  }
-  NumberOfAcini <-length(na.exclude(Data))
-  cat("For", Names[i], "we counted", NumberOfAcini, "Acini with a mean volume of", Mean, "\n")
-  }
-  
+  for (i in 2:length(Names)) {
+    cat("reading Sheet #",i,"\n")
+    Data <- read.xls(FileLocation,sheet=i) #read Sheet Nr. i
+    Data <- Data[,2] # only keep Volumes (in the second column)
+    Mean <- mean(Data,na.rm=TRUE) # Mean
+    RoundedMean <- sprintf("%.5f", Mean)
+    NumberOfAcini <-length(na.exclude(Data))
+    if (!is.nan(Mean)) {
+      Title <- paste(Names[i],"|", NumberOfAcini, "Acini | Mean:", RoundedMean, "\n")
+      plot(Data,type="b") 
+      title(Title)
+    }
+    cat("For", Names[i], "we counted", NumberOfAcini, "Acini with a mean volume of", Mean, "\n")
+    }
+}
