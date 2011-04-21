2240d352c5ac31650e0065a70d23b1b6c0bcb5a5
(no author) <(no author)@d6103094-951f-4e2d-9b0d-013b0bede56b>
2011-04-21 15:45:54 +0000

    - added Conditionals for Plotting and BoxPlotting so it's easier for printing
    git-svn-id: http://code.ana.unibe.ch/svn/R@12 d6103094-951f-4e2d-9b0d-013b0bede56b

--- AcinusPaper/AcinusDataFromXLSFiles.R
+++ AcinusPaper/AcinusDataFromXLSFiles.R
@@ -10,6 +10,8 @@ setwd("p:/doc/#R/AcinusPaper")
 library(gdata) # needed to read XLS-Files (needs an installation of PERL: http://www.perl.org/get.html)
 
 ## Setup stuff
+DoPlots=0       # 1 does the Plots, something else doen't
+DoBoxplots=1    # 1 does the BoxPlots, something else doen't
 Days <- c(04,10,21,36,60)
 
 # Days <- 04
@@ -37,9 +39,8 @@ for (currentDay in 1:length(Days)) { # Iterate trough the days
     for (i in 1:length(SheetNames)) print(SheetNames[i])
   
     cat("---\n")
-    cat("Extracting single Volumes\n")
-    
-    ## In a first loop, read out all the sheets in the file and plot the volume data 
+    ## In a first loop, read out all the sheets in the file and plot the volume data
+    cat("Extracting single Volumes\n")        
     for (i in 1:length(SheetNames)) { # Iterate through every sheet in the current XLS-File
         # cat("reading Sheet #",i," named '",SheetNames[i],"'\n",sep="")
         Data <- read.xls(FileLocation,sheet=i) # read Sheet Nr. i
@@ -47,49 +48,50 @@ for (currentDay in 1:length(Days)) { # Iterate trough the days
         NumberOfCounts[i] <- length(Data$Volume)
         NumberOfAcini[i] <- length(na.exclude(Data$Volume))
         if (!is.nan(Mean[i])) { # Plot the Data for Acini where we actually have Data (i.e., where Mean[i] is not empty)
-            plot(Data$Volume,type="b",col="red")
-            PlotTitle <- paste(SheetNames[i],"|", NumberOfAcini[i], "Acini | Mean:", sprintf("%.4f", Mean[i]), "\n")
-            abline(h=Mean[i], col = "gray60") # Plot Mean[i]
-            title(PlotTitle)
-        cat("For", SheetNames[i], "we counted",
-            NumberOfAcini[i], "Acini with a mean volume of",
-            Mean[i], "(and omitted",sum(is.na(Data$Volume)),"empty counts).\n") # Give out something to read in the console
-        } # end if Mean[i] is not empty
-        if (is.nan(Mean[i])) { # if Mean[i] is empty, we probably have an empty Sheet...
-        cat("For", SheetNames[i], "we counted no Acini, this means that the sheet is probably emtpy.\n")
-        } # end if Mean[i] *is* empty
-    } # end iterate through sheets
+            if (DoPlots==1) {
+                plot(Data$Volume,type="b",col="red")
+                PlotTitle <- paste(SheetNames[i],"|", NumberOfAcini[i], "Acini | Mean:", sprintf("%.4f", Mean[i]), "\n")
+                abline(h=Mean[i], col = "gray60") # Plot Mean[i]
+                title(PlotTitle)
+            }
+            cat("For", SheetNames[i], "we counted",
+                NumberOfAcini[i], "Acini with a mean volume of",
+                Mean[i], "(and omitted",sum(is.na(Data$Volume)),"empty counts).\n") # Give out something to read in the console
+            } # end if Mean[i] is not empty
+            if (is.nan(Mean[i])) { # if Mean[i] is empty, we probably have an empty Sheet...
+                cat("For", SheetNames[i], "we counted no Acini, this means that the sheet is probably emtpy.\n")
+            } # end if Mean[i] *is* empty
+        } # end iterate through sheets
 
-    cat("---\n")
-    cat("Extracting single Volumes and saving them into an array for boxplotting\n")
-    
-    ConcatenatedVolumes = array(NaN,c(max(NumberOfCounts),length(SheetNames))) # Initialize Array for Volumes
-    GlobalMean = mean(Mean,na.rm=TRUE)
-    
-    ## In a second loop below, read out all the data again (could probably be done in the same loop), concatenate into an Array and use this for a Boxplot  
-    for (i in 1:length(SheetNames)) { # Iterate through every sheet in the current XLS-File
-        cat("Working on sheet ",i,"/",length(SheetNames),"\n",sep="")
-        Data <- read.xls(FileLocation,sheet=i) # read Sheet Nr. i
-        ConcatenatedVolumes[1:length(Data$Volume),i] = Data$Volume # put just read Data into ith column of Array
-    } # end iterate through sheets
-    BoxPlotTitle <- paste(FileName,"| total Acini:", sum(NumberOfAcini), "| global Mean:", sprintf("%.4f", mean(Mean,na.rm=TRUE)), "\n")
-    boxplot(ConcatenatedVolumes,
-        notch=TRUE,
-        varwidth=TRUE,
-        col="lightgray",
-        outline=TRUE, # Plot plot outliers
-        names=SheetNames,
-        main=BoxPlotTitle)
-    abline(h=GlobalMean, col = "red")
-    BoxPlotTitle <- paste(FileName,"| total Acini:", sum(NumberOfAcini), "| global Mean:", sprintf("%.4f", mean(Mean,na.rm=TRUE)), "| NO OUTLIERS (->MEAN)!\n")
-    boxplot(ConcatenatedVolumes,
-        notch=TRUE,
-        varwidth=TRUE,
-        col="lightgray",
-        outline=FALSE, # Don't plot outliers
-        names=SheetNames,
-        main=BoxPlotTitle)
-    abline(h=GlobalMean, col = "red")    
-    cat("---\n")
-    
+    cat("---\n")    
+    ## In a second loop below, read out all the data again (could probably be done in the same loop), concatenate into an Array and use this for a Boxplot
+    if (DoBoxplots==1) {
+        cat("Extracting single Volumes and saving them into an array for boxplotting\n")
+        ConcatenatedVolumes = array(NaN,c(max(NumberOfCounts),length(SheetNames))) # Initialize Array for Volumes
+        GlobalMean = mean(Mean,na.rm=TRUE)        
+        for (i in 1:length(SheetNames)) { # Iterate through every sheet in the current XLS-File
+            cat("Working on sheet ",i,"/",length(SheetNames),"\n",sep="")
+            Data <- read.xls(FileLocation,sheet=i) # read Sheet Nr. i
+            ConcatenatedVolumes[1:length(Data$Volume),i] = Data$Volume # put just read Data into ith column of Array
+        } # end iterate through sheets
+        BoxPlotTitle <- paste(FileName,"| total Acini:", sum(NumberOfAcini), "| global Mean:", sprintf("%.4f", mean(Mean,na.rm=TRUE)), "\n")
+        boxplot(ConcatenatedVolumes,
+            notch=TRUE,
+            varwidth=TRUE,
+            col="lightgray",
+            outline=TRUE, # Plot plot outliers
+            names=SheetNames,
+            main=BoxPlotTitle)
+        abline(h=GlobalMean, col = "red")
+        BoxPlotTitle <- paste(FileName,"| total Acini:", sum(NumberOfAcini), "| global Mean:", sprintf("%.4f", mean(Mean,na.rm=TRUE)), "| NO OUTLIERS (->MEAN)!\n")
+        boxplot(ConcatenatedVolumes,
+            notch=TRUE,
+            varwidth=TRUE,
+            col="lightgray",
+            outline=FALSE, # Don't plot outliers
+            names=SheetNames,
+            main=BoxPlotTitle)
+        abline(h=GlobalMean, col = "red")    
+        cat("---\n")
+    }
 } # end iterate through Days
