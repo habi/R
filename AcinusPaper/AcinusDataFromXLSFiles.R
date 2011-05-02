2858af7ce0ea47800bd084bb4a222c5f92a2fce2
(no author) <(no author)@d6103094-951f-4e2d-9b0d-013b0bede56b>
2011-05-02 16:20:17 +0000

    - added info for TikZing - reading Volume from Stefans Ofiginal Datafile, so we know we have the correct info - prettified plot with y-Limits - Skeleton for dividing through Volume of RUL set, needs full implementation.
    git-svn-id: http://code.ana.unibe.ch/svn/R@15 d6103094-951f-4e2d-9b0d-013b0bede56b

--- AcinusPaper/AcinusDataFromXLSFiles.R
+++ AcinusPaper/AcinusDataFromXLSFiles.R
@@ -11,14 +11,17 @@ rm(list=ls()) # clear EVERYTHING!
 setwd("p:/doc/#R/AcinusPaper")
 library(gdata) # needed to read XLS-Files (needs an installation of PERL: http://www.perl.org/get.html)
 library(outliers)
+require(tikzDevice)
 
 ## Setup stuff
 DoPlots=0       # 1 does the Plots, something else doen't
 DoBoxplots=1    # 1 does the BoxPlots, something else doen't
-DoBoxplotsWithoutOutliers=0    # 1 does the BoxPlots without the Outliers, something else doen't
+UseOriginalFiles = 0 # Use original files WITH outliers, saved in p:\doc\#Tables\AcinarTreeExtraction\NodeHeight2-Originals\
+DivideThroughSize = 1
+
 Days <- c(04,10,21,36,60)
 
-Days <- 04
+# Days <- 04
 # Days <- 10
 # Days <- 21
 # Days <- 36
@@ -30,6 +33,7 @@ Mean <- NaN
 GlobalMean <- NaN
 NumberOfCounts <- NaN
 NumberOfAcini <- NaN
+TotalVolumes = array(NaN,c(750,length(Days))) # Initialize Array for TotalVolumes
 
 ## Loop through data in Days construct FileName for XLS-File
 for (currentDay in 1:length(Days)) { # Iterate trough the days
@@ -40,23 +44,16 @@ for (currentDay in 1:length(Days)) { # Iterate trough the days
     Day <- sprintf("%02.0f", Day) # Format the Day nicely, so we can read the Filenames correctly
     FileName <- paste("R108C",Day,".xls",sep="")
     FileLocation <- paste("p:/doc/#Tables/AcinarTreeExtraction/",FileName,sep="")
+    if (UseOriginalFiles == 1) {
+        FileLocation <- paste("p:/doc/#Tables/AcinarTreeExtraction/NodeHeight2-Originals/",FileName,sep="") # Use original xls-Files INCLUDING outliers
+    }
     SheetNames <- sheetNames(FileLocation) # Give out Information about read XLS-File
-    cat(FileName, "contains", sheetCount(FileLocation), "Sheets with the following names:\n")
-    for (i in 1:length(SheetNames)) print(SheetNames[i])
-  
     cat("---\n")
     ## In a first loop, read out all the sheets in the file and plot the volume data
     cat("Extracting single Volumes\n")        
     for (i in 1:length(SheetNames)) { # Iterate through every sheet in the current XLS-File
         # cat("reading Sheet #",i," named '",SheetNames[i],"'\n",sep="")
         Data <- read.xls(FileLocation,sheet=i) # read Sheet Nr. i
-###################### REMOVE OUTLIERS
-# NEEDS SOME MORE LOOKING INTO!
-#         if (!is.na(Data$Volume[1])) {
-#             Data$Volume<-rm.outlier(Data$Volume,fill=TRUE)
-#             cat("REMOVING OUTLIERS FOR", SheetNames[i], "\n")
-#         }
-###################### REMOVE OUTLIERS
         Mean[i] <- mean(Data$Volume,na.rm=TRUE) # Calculate the arithmetic mean without the empty cells and save into current Mean
         NumberOfCounts[i] <- length(Data$Volume)
         NumberOfAcini[i] <- length(na.exclude(Data$Volume))
@@ -81,11 +78,19 @@ for (currentDay in 1:length(Days)) { # Iterate trough the days
     if (DoBoxplots==1) {
         cat("Extracting single Volumes and saving them into an array for boxplotting\n")
         ConcatenatedVolumes = array(NaN,c(max(NumberOfCounts),length(SheetNames))) # Initialize Array for Volumes
-        GlobalMean[currentDay] = mean(Mean,na.rm=TRUE)        
+        GlobalMean[currentDay] = mean(Mean,na.rm=TRUE)
         for (i in 1:length(SheetNames)) { # Iterate through every sheet in the current XLS-File
             cat("Working on sheet ",i,"/",length(SheetNames),"\n",sep="")
             Data <- read.xls(FileLocation,sheet=i) # read Sheet Nr. i
             ConcatenatedVolumes[1:length(Data$Volume),i] = Data$Volume # put just read Data into ith column of Array
+            
+            #             if (DivideThroughSize == 1) {
+#                 ConcatenatedVolumes[,i]=ConcatenatedVolumes[,i]/StefansVolumes[currentDay,i]
+#             }
+            
+            
+            
+            TotalVolumes[1:length(c(ConcatenatedVolumes)),currentDay] <- c(ConcatenatedVolumes)
         } # end iterate through sheets
         BoxPlotTitle <- paste(FileName,"| total Acini:", sum(NumberOfAcini), "| global Mean:", sprintf("%.4f", mean(Mean,na.rm=TRUE)), "|", Time, "\n")
         boxplot(ConcatenatedVolumes,
@@ -96,21 +101,74 @@ for (currentDay in 1:length(Days)) { # Iterate trough the days
             names=SheetNames,
             main=BoxPlotTitle)
         abline(h=GlobalMean[currentDay], col = "red")
-        if (DoBoxplotsWithoutOutliers==1) {
-            BoxPlotTitle <- paste(FileName,"| total Acini:", sum(NumberOfAcini), "| global Mean:", sprintf("%.4f", mean(Mean,na.rm=TRUE)), "|", Time, "| NO OUTLIERS (->MEAN)!\n")
-            boxplot(ConcatenatedVolumes,
-                notch=TRUE,
-                varwidth=TRUE,
-                col="lightgray",
-                outline=FALSE, # Don't plot outliers
-                names=SheetNames,
-                main=BoxPlotTitle)
-            abline(h=GlobalMean[currentDay], col = "red")
-        }
         cat("---\n")
     }
 } # end iterate through Days
 
-GlobalMean
+## Read RUL-VOlumes from Stefans XLS-File to calculate with them afterwards.
+StefansVolumes = array(NaN,c(5,5),dimnames = list(c(1,2,3,4,5), c("4", "10", "21", "36", "60")))
+cat("Reading RUL-Volumes in 'Datenblattstefan.xls':Day 04 to StefansVolumes\n")
+TMP <- read.xls("Datenblattstefan.xls", sheet="DAY 4")
+StefansVolumes[,1]<-as.numeric(levels(TMP$X.5[11:15])[TMP$X.5[11:15]]) # was read in as Factor, according to http://is.gd/QHQpCH, this makes values numeric.
+cat("Reading RUL-Volumes in 'Datenblattstefan.xls':Day 10 to StefansVolumes\n")
+TMP <- read.xls("Datenblattstefan.xls", sheet="DAY 10")
+StefansVolumes[,2]<-as.numeric(levels(TMP$X.5[11:15])[TMP$X.5[11:15]])
+cat("Reading RUL-Volumes in 'Datenblattstefan.xls':Day 21 to StefansVolumes\n")
+TMP <- read.xls("Datenblattstefan.xls", sheet="DAY 21")
+StefansVolumes[,3]<-as.numeric(levels(TMP$X.5[11:15])[TMP$X.5[11:15]])
+cat("Reading RUL-Volumes in 'Datenblattstefan.xls':Day 36 to StefansVolumes\n")
+TMP <- read.xls("Datenblattstefan.xls", sheet="DAY 36")
+StefansVolumes[,4]<-as.numeric(levels(TMP$X.5[11:15])[TMP$X.5[11:15]])
+cat("Reading RUL-Volumes in 'Datenblattstefan.xls':Day 60 to StefansVolumes\n")
+TMP <- read.xls("Datenblattstefan.xls", sheet="DAY 60")
+StefansVolumes[,5]<-as.numeric(levels(TMP$X.5[11:15])[TMP$X.5[11:15]])
 
+# Plot Increase
 Increase <- GlobalMean/GlobalMean[1]
+
+# Stefans Data
+StefansVolumina <- c(
+    mean(StefansVolumes[,1]),
+    mean(StefansVolumes[,2]),
+    mean(StefansVolumes[,3]),
+    mean(StefansVolumes[,4]),
+    mean(StefansVolumes[,5])) # RUL from Datenblattstefan.xls
+StefansIncrease <- StefansVolumina / StefansVolumina[1] # from p:\doc\#R\AcinusPaper\Datenblattstefan.xls (-> MEAN "rul")
+
+## Plot Increase
+plot(Days,StefansIncrease,
+  ylim=c(0,max(max(Increase),max(StefansIncrease))),
+  type="b",
+  main="Increase of Volumes compared to Day 4",
+  xlab="Days",
+  ylab="Increase",
+  pch=1,
+  col="blue")
+par(new=TRUE) # actually don't make a new plot    O.o
+plot(Days,Increase,
+  ylim=c(0,max(max(Increase),max(StefansIncrease))),
+  type="b",
+  axes=FALSE,
+  main="",
+  xlab="",
+  ylab="",
+  pch=2,
+  col="red")
+legend(list(x=50,y=5),legend = c("RLL","Acini"),pch=1:2,lty=1,col=c("blue","red"))
+
+cat("Our increase is: ",sprintf("%.3f", Increase),"\n")
+cat("Stefans increase is: ",sprintf("%.3f", StefansIncrease),"\n")
+
+# tikz('_AcinarBoxPlots.tex', standAlone = TRUE)
+boxplot(TotalVolumes,
+  varwidth=TRUE,
+  notch=TRUE,
+  col="lightgray",
+  main="Boxplot of combined Acinar Volumes of all measurements",
+  xlab="Days after birth",
+  names=c(4,10,21,36,60)
+  )
+# dev.off()
+# cat("Saved Boxplot to _AcinarBoxPlots.tex in",getwd(),"\n")
+
+cat("---\n")
